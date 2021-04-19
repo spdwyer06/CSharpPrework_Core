d3c0f5d046005f0aca12898f66b6c1e04ea37459
Sean Dwyer <spdwyer06@gmail.com>
2021-04-19 19:43:59 -0400

    Add switch statements assembly

---
+++ 07_SwitchStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.WriteLine("How are you feeling today on a scale of 1-5?");
+int.TryParse(Console.ReadLine(), out int userFeeling);
+
+switch(userFeeling)
+{
+    case 5:
+        Console.WriteLine("That's great to hear!");
+        break;
+    case 4:
+        Console.WriteLine("Good stuff!");
+        break;
+    case 3:
+        Console.WriteLine("Hope things improve!");
+        break;
+    case 2: 
+        Console.WriteLine("Oh, sorry to hear that.");
+        break;
+    case 1:
+        Console.WriteLine("Dang, I hope your day gets better!");
+        break;
+    default:
+        Console.WriteLine("Enter a valid rating (1-5).");
+        break;
+}
