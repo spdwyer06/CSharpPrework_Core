c828290bba6447237ec9c8183312310ebf1745c6
Sean Dwyer <spdwyer06@gmail.com>
2021-04-19 17:30:37 -0400

    Add basic types

---
+++ 02_Types/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// bool is a true/false value, no quotes around the word 
+// this is because true/false are keywords, not strings
+bool isLoggedIn = true;
+Console.WriteLine(isLoggedIn);
+
+// integers, or int, are whole numbers only
+int age = 42;
+Console.WriteLine(age);
+
+// decimal has a lot of possible decimal points
+// it also needs the m suffix to know it's a decimal type
+decimal accountBalance = 9000.01m;
+Console.WriteLine(accountBalance);
