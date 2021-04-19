8bd73d20da13d1f3832790629272387acc2a0253
Sean Dwyer <spdwyer06@gmail.com>
2021-04-19 17:25:46 -0400

    Add variables assembly

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+string firstName = "Sean";
+Console.WriteLine(firstName);
+
+Console.WriteLine("What is your favorite color?");
+string colorInput = Console.ReadLine();
+Console.WriteLine("You said your favorite color is " + colorInput);
+
+Console.WriteLine("What is your first name?");
+firstName = Console.ReadLine();
+Console.WriteLine($"You said your first name is {firstName}");
