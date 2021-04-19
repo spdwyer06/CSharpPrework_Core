dda554ab10a5f481ee06bac847dc6183e04ee11a
Sean Dwyer <spdwyer06@gmail.com>
2021-04-19 18:09:09 -0400

    Add operators assembly

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/* ARITHMETIC OPERATORS */
+
+int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 = " + additionResult);
+
+int subtractionResult = 5 - 2;
+Console.WriteLine("5 - 2 = " + subtractionResult);
+
+int muliplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + muliplicationResult);
+
+int divisionResult = 5 / 2;
+Console.WriteLine("5 / 2 = " + divisionResult);
+
+int remainderResult = 5 % 2;
+Console.WriteLine("5 % 2 = " + remainderResult);
+
+int noRemainder = 9 & 3;
+Console.WriteLine("9 % 3 = " + noRemainder);
